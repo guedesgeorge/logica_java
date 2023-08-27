f9e21d766474886a7cfd14ae4a77d3fef7571dbd
´´George <george.emannuelcarvalho@gmail.com>
2023-08-27 13:52:45 -0300

    Primeiro commit

---
+++ curso_logica/src/curso_logica/Main.java
@@ -0,0 +1,26 @@
+package curso_logica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int idade;
+		double salario, altura;
+		char genero;
+		String nome;
+		
+		idade = 30;
+		salario = 5000;
+		altura = 1.85;
+	    genero  = 'F';
+	    nome = "Maria Silva";
+		
+		System.out.println("IDADE = " + idade);
+		System.out.println("SALARIO = " + salario);
+		System.out.println("ALTURA = " + altura);
+		System.out.println("NOME = " + nome);
+		
+
+	}
+
+}
